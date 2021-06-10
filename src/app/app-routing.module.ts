a237664c7c5850a866d3705dff545fb7e0099f3f
RGAC98 <59809286+RGAC98@users.noreply.github.com>
2021-06-10 02:57:17 -0500

    feat: componente proveedores
    Se ha creado el componente de proveedores y se ha desarrollado los métodos de crear proveedor y obtener proveedores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import {ProveedoresComponent} from '../app/proveedor/proveedores/proveedores.component'
 import { CabeceraClienteComponent } from "./factura_cabecera/cabecera-cliente/cabecera-cliente.component";
 
 const routes: Routes = [
-  {path: 'cabeceras', component: CabeceraClienteComponent}
+  {path: 'cabeceras', component: CabeceraClienteComponent},
+  {path: 'proveedores', component: ProveedoresComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms'
 
 //Aqui (shared/material/material.module) importar todos los modulos de material que se requiera, aunque ya estan todos casi.
 import { MaterialModule } from './shared/material/material.module';
-
 import { HttpClientModule } from '@angular/common/http';
 
 import { CabeceraClienteComponent } from './factura_cabecera/cabecera-cliente/cabecera-cliente.component';
+import { ProveedoresComponent } from './proveedor/proveedores/proveedores.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraClienteComponent
+    CabeceraClienteComponent,
+    ProveedoresComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
