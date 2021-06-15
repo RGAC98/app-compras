5b937af471f02e50379c63888908e93fa3b626cb
RGAC98 <59809286+RGAC98@users.noreply.github.com>
2021-06-14 23:30:47 -0500

    feat: consumo de api inventario
    Se ha creado un método mediante el cual se consumirá el api de inventario

--- src/app/factura_producto/inventario/inventario.component.ts
+++ src/app/factura_producto/inventario/inventario.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog'
-
+import {ApiInventarioService} from '../../_services/api-inventario.service'
 
 @Component({
   selector: 'app-inventario',
@@ -10,16 +10,30 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog'
 
 export class InventarioComponent implements OnInit 
 {
+  //variables para almacenar el mensaje del dialog
   mensaje: string
 
-  constructor(@Inject(MAT_DIALOG_DATA) private data: DataDialog) 
+  //variable para obtener inventario del api
+  inventario: Array<any> = []
+
+  constructor(@Inject(MAT_DIALOG_DATA) private data: DataDialog, private inventario_api: ApiInventarioService) 
   {
     this.mensaje = this.data.mensaje
     console.log(this.mensaje);
+    this.ObtenerInventario()
   }
 
   ngOnInit(): void 
   {}
+
+  ObtenerInventario()
+  {
+    this.inventario_api.getObtenerInventario().subscribe((respuesta: any) => {
+      this.inventario = respuesta
+      console.log(this.inventario);
+      
+    })
+  }
 }
 
 export interface DataDialog
